d58904cd7455e27c76159a2de36026e570fb7df1
seoin <inguk920@naver.com>
2017-01-18 19:18:02 +0900

    login_mainwindow

--- src/GlobalValue/GlobalValue.java
+++ src/GlobalValue/GlobalValue.java
@@ -1,8 +1,16 @@
 package GlobalValue;
 
+import java.awt.Font;
+
 public class GlobalValue {
-	public static final int nLoginFrameWidth = 1280;
-	public static final int nLoginFrameHeight = 720;
+	public static final int nLoginFrameWidth = 450;
+	public static final int nLoginFrameHeight = 750;
+	
+	public static final int nButtonx = nLoginFrameWidth/6;
+	public static final int nButtony = nLoginFrameHeight*4/7;
 	
 	public static final int nLoginTitle = nLoginFrameHeight/2;
+	public static final Font Font_Label = new Font("휴먼편지체",Font.PLAIN, 18);
+	public static final Font Font_Button = new Font("휴먼편지체",Font.BOLD, 18);
+	public static final Font Font_PName = new Font("휴먼편지체",Font.BOLD,24);
 }
--- src/Main/Frame/MainFrame.java
+++ src/Main/Frame/MainFrame.java
@@ -1,23 +1,38 @@
 package Main.Frame;
 
 import GlobalValue.GlobalValue;
-
 import java.awt.*;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.Font;
+
 import javax.swing.*;
 // java.awt 랑 javax.swing은 그래픽 라이브러리
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements ActionListener,MouseListener{
 	private int nWidth;
 	private int nHeight;
-	private JLabel Label_ID,Label_Password;
+	private JLabel Label_ID,Label_Password, Label_Image, Label_PName;
+	private ImageIcon Label_ImageIcon;
 	private JTextField TextField_ID,TextField_Password;
-	private JPanel LabelPanel;
+	private JPanel LoginPanel;
+	private JButton Button1;
+	private String Input_ID;
+	private String Input_Password;
+	
 	
 	public MainFrame() {
 		this.setTitle("Log-In");
 		this.setPreferredSize(new Dimension(GlobalValue.nLoginFrameWidth,GlobalValue.nLoginFrameHeight));
 		this.setBackground(Color.black);
 		this.setVisible(true);
+		this.setResizable(false);
+
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		// 프레임의 크기를 설정
 		// 프레임의 배경색을 설정
@@ -25,34 +40,71 @@ public class MainFrame extends JFrame {
 		// 프레임의 x버튼을 누르면 어떻게 할껀지 설정
 		
 		
-		LabelPanel = new JPanel();
+		LoginPanel = new JPanel();
+		LoginPanel.setLayout(null);
+		LoginPanel.setBackground(Color.WHITE);
 		//프레임위에서 사용할 패널을 정의한다.
+		Label_ImageIcon = new ImageIcon("image/logo.jpg");
+		Label_Image = new JLabel(Label_ImageIcon);
+		Label_Image.setBounds(GlobalValue.nButtonx+25,10,250,250);
+		LoginPanel.add(Label_Image);
 		
-		Label_ID = new JLabel("학번 :");
-		Label_ID.setHorizontalAlignment(SwingConstants.CENTER);
-		LabelPanel.add(Label_ID);
-		//라벨을 만들어서 패널에 추가한다.
+		Label_PName = new JLabel("설현 내꺼");
+		Label_PName.setFont(GlobalValue.Font_PName);
+		Label_PName.setBounds(GlobalValue.nButtonx+100,GlobalValue.nButtony-160,100,50);
+		LoginPanel.add(Label_PName);
+		
+		Label_ID = new JLabel("아이디(학번)");
+		Label_ID.setFont(GlobalValue.Font_Label);
+		Label_ID.setBounds(GlobalValue.nButtonx,GlobalValue.nButtony-100,100,50);
+		LoginPanel.add(Label_ID);
 		
 		TextField_ID = new JTextField(10);
-		TextField_ID.setHorizontalAlignment(SwingConstants.CENTER);
-		LabelPanel.add(TextField_ID);
+	//	TextField_ID.addMouseListener(this);
+		TextField_ID.setBounds(GlobalValue.nButtonx + 100,GlobalValue.nButtony-100,200,50);
+		LoginPanel.add(TextField_ID);
+		//텍스트영역을 만들어서 패널에 추가한다.
+
+		
+		Label_Password = new JLabel("비밀번호");
+		Label_Password.setFont(GlobalValue.Font_Label);
+		Label_Password.setBounds(GlobalValue.nButtonx+30,GlobalValue.nButtony-50,100,50);
+		LoginPanel.add(Label_Password);
+		
+		TextField_Password = new JPasswordField(10);
+		TextField_Password.setBounds(GlobalValue.nButtonx+100,GlobalValue.nButtony-50,200,50);
+		LoginPanel.add(TextField_Password);
+		
 		//텍스트영역을 만들어서 패널에 추가한다.
 		
-		Label_Password = new JLabel("비번 :");
-		Label_Password.setHorizontalAlignment(SwingConstants.CENTER);
-		LabelPanel.add(Label_Password);
-		//라벨을 만들어서 패널에 추가한다.
-		
-		TextField_Password = new JTextField(10);
-		TextField_Password.setHorizontalAlignment(SwingConstants.CENTER);
-		LabelPanel.add(TextField_Password);
-		//텍스트영역을 만들어서 패널에 추가한다.
-		
-		this.add(LabelPanel);
+		Button1 = new JButton("로그인");
+		Button1.setBounds(GlobalValue.nButtonx,GlobalValue.nButtony+20,300,50);
+		Button1.addActionListener(this);
+		Button1.setFont(GlobalValue.Font_Button);
+		Button1.setBackground(Color.DARK_GRAY);
+		Button1.setForeground(Color.WHITE);
+		LoginPanel.add(Button1);
+
+		this.add(LoginPanel);
 		//패널을 프레임에 추가한다.
 			
 		this.pack();
 	}
-	
+	public void actionPerformed(ActionEvent event) {
+		Input_ID = TextField_ID.getText();
+		System.out.println(Input_ID);
+		Input_Password = TextField_Password.getText();
+		System.out.println(Input_Password);
+	}
+	public void mouseDragged(MouseEvent event) {}
+	public void mouseMoved(MouseEvent event) {}
+	public void mousePressed(MouseEvent event) {}
+	public void mouseReleased(MouseEvent event) {}
+	public void mouseEntered(MouseEvent event) {}
+	public void mouseExited(MouseEvent event) {}
+	public void mouseClicked(MouseEvent event) {
+	//	TextField_ID.setText(null);
+	}
+
 	
 }
